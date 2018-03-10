18b8ba81c2ff20b7d317a6a121801ea3adaaabff
Swati Garg <snayha@Swatis-MacBook-Air.local>
2018-03-10 14:57:14 -0600

    communication

---
+++ unity/Communication/Assets/Code/Example.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour {
+	public string postURL = "helloworld.net/post.php";
+	public string getURL = "helloworld.net/get.php";
+	private string player = "";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
