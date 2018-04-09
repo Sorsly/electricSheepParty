ec2855be08d8f9634c4d796f77e0053a1fcf96b1
clewelli <clewelli@purdue.edu>
2018-04-08 20:05:47 -0400

    move april-8 2

--- bots/main/udp.c
+++ bots/main/udp.c
@@ -215,52 +215,36 @@ void move(commands * cmd, resp *state){
         des_angle+=360;
     }
     //double curr_angle = getRawTheta(startXorient,startYorient,&xMag,&yMag);
-<<<<<<< HEAD
-    double curr_angle=cmd->camorient+180;//adjust for camera's angle
-    if (curr_angle>360){
-	curr_angle+=-360;
-    } else if (curr_angle<0){
-	curr_angle+=360;
-    }
+    double curr_angle=cmd->camorient;//adjust for camera's angle
 
-=======
-    double curr_angle=cmd->camorient;
-    printf("%f %f",des_angle,curr_angle);
->>>>>>> 3f69e2b8121960c69d361ded253f5a131e960943
     if (abs(des_angle-curr_angle)>5){
 	double delt_angle=curr_angle-des_angle;
 	int turntime=abs(200*delt_angle/240);
 	if (delt_angle>0){
 		//turn right
-        left_ctl(false,cmd->twiddleR);
-        right_ctl(true,cmd->twiddleL);
+        	left_ctl(false,cmd->twiddleR);
+        	right_ctl(true,cmd->twiddleL);
 		vTaskDelay(turntime);
 	}else{	
-        left_ctl(true, cmd->twiddleL);
-        right_ctl(false, cmd->twiddleR);
+        	left_ctl(true, cmd->twiddleL);
+        	right_ctl(false, cmd->twiddleR);
 		vTaskDelay(turntime);
 	}
     }
-
-    double x2=pow(cmd->relDesX,2);
-    double y2=pow(cmd->relDesY,2);
-    printf("x2 %f y2 %f",x2,y2);
-    double hyp=sqrt(x2+y2);
-<<<<<<< HEAD
-    if (hyp>4&&hyp<8){
-	left_ctl(false,cmd->twiddleL);
-	right_ctl(false,cmd->twiddleR);
-	vTaskDelay(20);//move approximately 2 cm
-    } else if(hyp>8){
-	left_ctl(false,cmd->twiddleL);
-	right_ctl(false,cmd->twiddleR);
-	vTaskDelay(60);
-=======
-    if (hyp>30){
-	left_ctl(true,cmd->twiddleL);
-	right_ctl(true,cmd->twiddleR);
-	vTaskDelay(20);
->>>>>>> 3f69e2b8121960c69d361ded253f5a131e960943
+    if (abs(des_angle-Curr_angle)<45){
+    	double x2=pow(cmd->relDesX,2);
+    	double y2=pow(cmd->relDesY,2);
+    	printf("x2 %f y2 %f",x2,y2);
+   	double hyp=sqrt(x2+y2);
+    	if (hyp>4&&hyp<8){
+		left_ctl(false,cmd->twiddleL);
+		right_ctl(false,cmd->twiddleR);
+		vTaskDelay(20);//move approximately 2 cm
+    	} else if(hyp>8){
+		left_ctl(false,cmd->twiddleL);
+		right_ctl(false,cmd->twiddleR);
+		vTaskDelay(60);
+	}
     }
 }
 
